055849277da6d7f043e4939da0dc6cb1a1a25fb1
Vladimir Kotal <vlada@kotalovi.cz>
2022-06-21 23:30:53 +0200

    iaq_init() already called from Adafruit_SGP30()

--- examples/sgp30_simpletest.py
+++ examples/sgp30_simpletest.py
@@ -15,7 +15,6 @@ sgp30 = adafruit_sgp30.Adafruit_SGP30(i2c)
 
 print("SGP30 serial #", [hex(i) for i in sgp30.serial])
 
-sgp30.iaq_init()
 sgp30.set_iaq_baseline(0x8973, 0x8AAE)
 sgp30.set_iaq_relative_humidity(celcius=22.1, relative_humidity=44)
 
